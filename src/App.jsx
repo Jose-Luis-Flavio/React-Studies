fdd28a3751c5206ef45ae1231968d5cfba58b6c3
Jose-Luis-Flavio <joseluis.bmjnet@gmail.com>
2026-03-24 11:45:14 -0300

    PropSyntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -38,20 +38,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-            title={CORE_CONCEPTS[0].title} 
-            description={CORE_CONCEPTS[0].description} 
-            img={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            img={CORE_CONCEPTS[1].image} />
-            <CoreConcept title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            img={CORE_CONCEPTS[2].image} />
-            <CoreConcept title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-            img={CORE_CONCEPTS[3].image} />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
